a978b53fa1f009fa2f22eeb54ecd5c4082cc1fcb
JeongMinU <jkmn0823@gmail.com>
2023-06-11 04:16:09 +0900

    간략한 코드 추가

--- 로또번호 추출.js
+++ 로또번호 추출.js
@@ -16,3 +16,15 @@ function calculateAverage() {
 
 // 함수 호출
 calculateAverage();
+/////////////////////////////////////////////////////////제가 조금 더 간단한 코드를 짜봤습니다.
+
+// 두 수를 입력받아 더하는 함수
+function addNumbers() {
+  var num1 = parseInt(prompt("첫 번째 숫자를 입력하세요:"));
+  var num2 = parseInt(prompt("두 번째 숫자를 입력하세요:"));
+  var sum = num1 + num2;
+  console.log("두 수의 합은 " + sum + "입니다.");
+}
+
+// 함수 호출
+addNumbers();
