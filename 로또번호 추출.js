fd4d5d386aa3f28bb4d1a6a73d95dd924c8ae1f0
gitminu1 <jkm0148@naver.com>
2023-06-11 04:47:48 +0900

    Update 로또번호 추출.js

--- 로또번호 추출.js
+++ 로또번호 추출.js
@@ -16,3 +16,19 @@ function calculateAverage() {
 
 // 함수 호출
 calculateAverage();
+
+calculateAverage();   //--------------------------그럼 이 부분은 어떻게 해결하는게 좋을까요??
+
+
+/////////////////////////////////////////////////////////제가 조금 더 간단한 코드를 짜봤습니다.
+
+// 두 수를 입력받아 더하는 함수
+function addNumbers() {
+  var num1 = parseInt(prompt("첫 번째 숫자를 입력하세요:"));
+  var num2 = parseInt(prompt("두 번째 숫자를 입력하세요:"));
+  var sum = num1 + num2;
+  console.log("두 수의 합은 " + sum + "입니다.");
+}
+
+// 함수 호출
+addNumbers(); //////////////////////////////////풀 리퀘스트를 위해 잠시 브랜치를 옮김
